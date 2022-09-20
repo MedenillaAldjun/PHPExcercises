764f4cb8eaf29a75bad97e63bc83cbdab0bdf8da
MedenillaAldjun <medenillaaldjun18@gmail.com>
2022-09-20 16:58:12 +0800

    Initial commit for ex5-7

---
+++ ex6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Exercise 6</title>
+</head>
+<body>
+<?php
+        for($x = 1; $x <= 12; $x++) {
+            $res = $x * $x;
+            echo "$x * $x = $res. <br>";
+        }
+    ?>
+</body>
+</html>
